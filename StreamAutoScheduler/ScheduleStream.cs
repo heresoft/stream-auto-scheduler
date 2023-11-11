39d5e41e38ac340208c9165ac7929a72a2665734
David Margetts <dgmargetts@hotmail.com>
2023-11-11 12:23:05 +0000

    More bits

--- StreamAutoScheduler/ScheduleStream.cs
+++ StreamAutoScheduler/ScheduleStream.cs
@@ -10,6 +10,15 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Data;
+using System.Web.Http;
 
 namespace StreamAutoScheduler
 {
@@ -25,25 +34,105 @@ namespace StreamAutoScheduler
         [FunctionName("ScheduleStream")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiRequestBody("application/json", typeof(ScheduleStreamRequestBodyModel), Description = "JSON request body containing {Title, Descritpion, StartTime}")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string title;
+            string description;
+            DateTime startTime;
+            ClientSecrets secrets;
+            TokenResponse token;
+            UserCredential credentials;
+            GoogleAuthorizationCodeFlow codeFlow;
+            GoogleAuthorizationCodeFlow.Initializer authInit;
+            BaseClientService.Initializer serviceInit;
+            YouTubeService service;
+            LiveBroadcast broadcast;
+            LiveBroadcastSnippet broadcastSnippet;
+            LiveBroadcastStatus broadcastStatus;
+            LiveBroadcastsResource.InsertRequest request;
+            LiveBroadcast response;
+            DateTimeOffset broadcastStartTime;
 
-            string name = req.Query["name"];
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            if (data == null)
+            {
+                return new BadRequestObjectResult("The request body does not contain a valid json object.");
+            }
+
+            title = data.title;
+            if (string.IsNullOrEmpty(title))
+            {
+                return new BadRequestObjectResult("Connection string is not set.");
+            }
+
+            CourtDirectoryAndFileName = data.courtDirectoryAndFileName;
+            if (string.IsNullOrEmpty(CourtDirectoryAndFileName))
+            {
+                return new BadRequestObjectResult("The court directory and file name is not set.");
+            }
+
+            FileName = data.fileName;
+            if (string.IsNullOrEmpty(FileName))
+            {
+                return new ExceptionResult(new Exception("The file name is not set"), true);
+            }
+
+            secrets = new ClientSecrets()
+            {
+                ClientId = "Client ID String",
+                ClientSecret = "Client Secret"
+            };
+
+            token = new TokenResponse { RefreshToken = "Refresh Token" };
+            authInit = new GoogleAuthorizationCodeFlow.Initializer { ClientSecrets = secrets };
+            codeFlow = new GoogleAuthorizationCodeFlow(authInit);
+            credentials = new UserCredential(codeFlow, "user", token);
+
+            serviceInit = new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credentials,
+                ApplicationName = "your-app-name"
+            };
+
+            service = new YouTubeService(serviceInit);
+
+            broadcastStartTime = new DateTimeOffset(startTime);
+
+            broadcastSnippet = new LiveBroadcastSnippet
+            {
+                Title = title,
+                ScheduledStartTimeDateTimeOffset = broadcastStartTime
+            };
+
+            broadcastStatus = new LiveBroadcastStatus { PrivacyStatus = "public" };
+
+            broadcast = new LiveBroadcast
+            {
+                Kind = "youtube#liveBroadcast",
+                Snippet = broadcastSnippet,
+                Status = broadcastStatus
+            };
+
+            request = service.LiveBroadcasts.Insert(broadcast, "id,snippet,status");
+            response = request.Execute();
+            //return response.Id;
 
             return new OkObjectResult(responseMessage);
         }
+    }
+
+    public class ScheduleStreamRequestBodyModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
     }
 }
 
